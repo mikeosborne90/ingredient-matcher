9a25450132836d2fe66ca82d220d22d029664d78
mikeosborne90 <mikeosborne1990@gmail.com>
2019-09-02 13:38:49 -0700

    Added ingredient pictures.

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,10 @@
+let selectedIngredients = [];
+
 function addIngredientToList()
 {
-    const ingredientList = document.getElementById("ingredientList"); //our ul from html
     let ingredientName = document.getElementById("ingredientName"); //user inputted ingredient
-    let li = document.createElement("li"); //create new li in html
 
-    li.appendChild(document.createTextNode(ingredientName.value)); //add ingredient to li
-    ingredientList.appendChild(li); //add li to ul
+    selectedIngredients.push(ingredientName);
+
+    console.log(selectedIngredients);
 }
